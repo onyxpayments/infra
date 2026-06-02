9962560ddbfeca7eef07413d63eb9f4f849ece79
Juan D. Bello <jdbelloro@unal.edu.co>
2026-06-01 20:41:32 -0500

    sql updated

--- postgres/transactions/init/01_create_transactions.sql
+++ postgres/transactions/init/01_create_transactions.sql
@@ -28,9 +28,9 @@ CREATE TABLE transactions (
     customer_first_name VARCHAR(100) NOT NULL,
     customer_last_name VARCHAR(100) NOT NULL,
     customer_personal_id VARCHAR(50) NOT NULL,
-    customer_email VARCHAR(255) NOT NULL,
-    customer_country VARCHAR(2) NOT NULL,
-    customer_ip INET NOT NULL,
+    -- customer_email VARCHAR(255) NOT NULL,
+    -- bcustomer_country VARCHAR(2) NOT NULL,
+    -- customer_ip INET NOT NULL,
 
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
